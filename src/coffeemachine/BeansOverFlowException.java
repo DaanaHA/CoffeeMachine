c4e424895f5e0a01c2ad7d894e684048a62d915c
DaanaHA <danaheyam2@gmail.com>
2023-05-11 19:30:25 +0300

    befor GUI

--- src/coffeemachine/BeansOverFlowException.java
+++
@@ -1,9 +0,0 @@
-package coffeemachine;
-
-public class BeansOverFlowException extends Exception{
-
-    public BeansOverFlowException(String string) {
-        super(string);
-    }
-    
-}
--- src/coffeemachine/WaterOverFlowException.java
+++
@@ -1,9 +0,0 @@
-package coffeemachine;
-
-public class WaterOverFlowException extends Exception{
-
-    public WaterOverFlowException(String string) {
-        super(string);
-    }
-    
-}
